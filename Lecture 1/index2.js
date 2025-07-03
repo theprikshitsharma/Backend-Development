dc872461f79ef99d3c3ca804e4c08767f277eb32
theprikshitsharma <theprikshitsharma@gmail.com>
2025-07-03 15:35:40 +0530

    lecture 1

---
+++ Lecture 1/index2.js
@@ -0,0 +1,36 @@
+const fs=require("fs");
+const { nextTick } = require("process");
+console.log(fs);
+console.log("start");
+
+setImmediate(()=>{
+    console.log("set Immediate")
+})
+setTimeout(()=>{
+    console.log("settimeout")
+},0)
+fs.readFile("demo.txt",(data)=>{
+    console.log("file read")
+    setTimeout(()=>{
+        console.log("timer 2");
+    },0)
+    setImmediate(()=>{
+        console.log("immediate 2");
+    })
+})
+function someTask(){
+    return new Promise((resolve,reject)=>{
+        resolve("promise")
+    })
+}
+someTask().then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
+process.nextTick(()=>{
+    console.log("next tick")
+})
+
+console.log("end");
